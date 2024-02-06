a5a150127ebb49206228f17b0e6260a7f9f7c347
LenaKadilova <138900490+LenaKadilova@users.noreply.github.com>
2024-02-06 16:21:07 +0400

    Update main.py
    № 4

--- main.py
+++ main.py
@@ -2,27 +2,35 @@ import sys
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QWidget, QLabel, QApplication
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QComboBox
 import requests
-
-
-
 
 
 class Img(QWidget):
     def __init__(self):
         super().__init__()
         self.label = QLabel(self)
+        self.combobox = QComboBox(self)
+        self.combobox.addItems(['схема', 'спутник', 'гибрид'])
+        self.combobox.currentTextChanged.connect(self.show_img)
+        self.map = 'map'
         self.spn = '0.05'
         self.longitude, self.lattitude = '37.6156', '55.7522'
         self.show_img()
         self.show()
 
     def show_img(self):
+        print(self.combobox.currentText())
+        # if self.combobox.currentText() == 'схема':
+        #     self.map = 'map'
+        # elif self.combobox.currentText() == 'спутник':
+        #     self.map = 'satellite'
+        # elif self.combobox.currentText() == 'гибрид':
+        #     self.map = 'hybrid'
         map_params = {
             "ll": ",".join([self.longitude, self.lattitude]),
             "spn": ",".join([self.spn, self.spn]),
-            "l": "map",
+            "l": self.map,
         }
         map_api_server = "http://static-maps.yandex.ru/1.x/"
         response = requests.get(map_api_server, params=map_params)
